a333f6678b5b872efe549a3833ed857749f12948
kiriketsuki <kiriketsuki@gmail.com>
2021-09-12 04:38:05 +0800

    figured out package compiling error

--- lab1/P2.java
+++ lab1/P2.java
@@ -1,4 +1,3 @@
-package lab1;
 import java.util.*;
 
 public class P2 {
--- lab1/my_first_program.java
+++ lab1/my_first_program.java
@@ -1,4 +1,3 @@
-package lab1;
 public class my_first_program {
     public static void main(String[] args) {
         System.out.println("Hello! This is my first java program");
